6d64aa9e37ee1982a6aba883f0b80dff1189314d
김동영 <32670163+DongyoungKim2@users.noreply.github.com>
2023-06-03 23:02:52 +0900

    add google analytics

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-PDLHHVCPF5"></script>
+        <script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-PDLHHVCPF5', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body className={inter.className}>
         <Navbar />
         <div className='z-20	'>
